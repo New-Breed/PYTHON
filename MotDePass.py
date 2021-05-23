61191a460855c6c22a5ae9d5089502c920061297
Benoit Beauchemin <bbeauchemin89@hotmail.com>
2021-05-23 14:41:52 -0400

    V2

---
+++ MotDePass.py
@@ -0,0 +1,22 @@
+#coding:utf-8
+
+Log1 = "Ben"
+Pass1 = "123"
+
+print("Connextion")
+
+Log = input ("Login : ")
+if Log == Log1 :
+	print("Ok")
+
+	Pass = input ("Pass : ")
+	if Pass == Pass1 :
+		print("Voila")
+	
+	else:
+	print("NOK")
+	
+else:
+	print("NOK")
+
+print(" ... ")
